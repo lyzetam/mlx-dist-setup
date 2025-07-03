97cd886a24d13894a830a8d454d78f6e9df330d3
lyzetam <62520358+lztech-tp@users.noreply.github.com>
2025-07-03 00:46:42 -0400

    originial nb

---
+++ distributed_infer.py
@@ -0,0 +1,16 @@
+import mlx.core as mx
+from mlx_lm import load, generate
+import socket
+
+world = mx.distributed.init()
+rank = world.rank()
+size = world.size()
+mx.set_default_device(mx.gpu)
+
+if rank == 0:
+    print(f'Running on {size} processes')
+
+model, tokenizer = load('mlx-community/Llama-3.2-1B-Instruct-4bit')
+prompt = f'Hello from rank {rank}!'
+result = generate(model, tokenizer, prompt, max_tokens=20)
+print(f'[{rank}/{size} on {socket.gethostname()}] {result}')
